6b1283b319a787ef5333fb18258d977ffb334e9b
ademch <ntuu.kpi@ukr.net>
2026-01-29 12:36:54 +0200

    freshup after 5 years

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '/src/public/index.html'),
+      template: path.resolve(__dirname, 'src/public/index.html'),
       inject: 'body'
     }),
     new MiniCssExtractPlugin()
